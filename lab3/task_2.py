ab77cef95916600547447db8eb08f43983d7ff95
KozlovNA <kozlov.na@phystech.edu>
2021-09-27 12:04:21 +0300

    update task 2

--- lab3/task_2.py
+++ lab3/task_2.py
@@ -8,7 +8,13 @@ FPS = 30
 screen = pygame.display.set_mode((500, 500))
 
 #code
-rect(0, 0, 500, 250)
+rect(screen, (66, 170, 255), (0, 0, 500, 250), 0)  # sky
+rect(screen, (0, 128, 0), (0, 250, 500, 250), 0)  # grass
+rect(screen, (150, 75, 0), (100, 200, 150, 100), 0)  # house
+polygon(screen, (255, 0, 0), [(175, 125), (100, 200), (250, 200)], 0)  # roof
+rect(screen, (150, 75, 0), (350, 220, 15, 60), 0)  # log
+
+
 #code
 
 pygame.display.update()
